d8583b4dda624f928d85247e61a4090d3d83562b
Luisa1201 <lfacostav@ufpso.edu.co>
2025-09-06 17:40:30 +0200

    Version 4 - table

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import HookUserState from './playground/HookUserState'
+import HookUseState from './playground/HookUseState'
 import HomeHooks from './playground/HomeHooks'
 import HookUseNavigate from './playground/HookUseNavigate'
 
@@ -10,7 +10,7 @@ function App() {
    <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomeHooks />}></Route>
-         <Route path='/useState' element={<HookUserState />}></Route>
+         <Route path='/useState' element={<HookUseState />}></Route>
          <Route path='/useNavigate' element={<HookUseNavigate />}></Route>
       </Routes>
     </BrowserRouter>
